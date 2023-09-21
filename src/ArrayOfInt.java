bc7e374f5a39761845aec501bd5cad1959e56c37
AsherBasheer <asherbasheer.ca@gmail.com>
2023-09-21 11:31:19 -0400

    .

--- src/ArrayOfInt.java
+++ src/ArrayOfInt.java
@@ -1,23 +1,28 @@
 import static java.lang.Math.random;
-
+import java.util.Scanner;
 public class ArrayOfInt {
     public static void main(String[] args){
-        int[] arr=new int[10];
-        for(int i=0;i<10;i++){
-
-            arr[i]=(int) (Math.random()*10);
-            System.out.println(arr[i]);
+        int i=0;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the total number of numbers to be checked");
+        int input=scan.nextInt();
+        int[] arr=new int[input];
+        System.out.println("Enter "+input+ " numbers one by one");
+        for( i=0;i<input;i++){
+            arr[i]=scan.nextInt();
+            //arr[i]=(int) (Math.random()*10);
+            //System.out.println(arr[i]);
         }
         int y=0;
         int large=arr[0];
         int small=arr[0];
-        while(y<9){
-            if(large<arr[y+1]){
+        while(y<input){
+            if(large<arr[y]){
 
-                large=arr[y+1];
+                large=arr[y];
             }
-            if(small>arr[y+1]){
-                small=arr[y+1];
+            if(small>arr[y]){
+                small=arr[y];
             }
             y+=1;
 
